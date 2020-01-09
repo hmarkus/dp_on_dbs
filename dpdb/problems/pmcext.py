9cdd4dc97c0b214ebd2c79cfde1ccdaacc062ff1
Markus Hecher <hecher@cobra-submit.kr.tuwien.ac.at>
2020-01-09 12:19:35 +0100

    updated asp encodings/better suited to actual abstraction...

--- dpdb/problems/pmcext.py
+++ dpdb/problems/pmcext.py
@@ -115,8 +115,8 @@ class PmcExt(Problem):
                     else:
                         clauses.append([n*(-1)])
                         extra_clauses.append(n*(-1))
-            sat = self.abstr.solve_external(self.num_vars,clauses,extra_clauses,set(covered_vars).intersection(self.projected))
             if not self.interrupted:
+                sat = self.abstr.solve_external(self.num_vars,clauses,extra_clauses,set(covered_vars).intersection(self.projected))
                 db.update(f"td_node_{node.id}",["model_count"],["model_count * {}".format(sat)],where)
         except Exception as e:
             raise e
